5573bb9ee6a4db58eacf1e82b81c088f43778896
Rafał Kostrzewski <poczta.raf.kos@gmail.com>
2017-05-27 17:56:56 +0200

    feature(PreactConfig): Helpers for webpack config transforms

--- src/lib/transform-config.js
+++ src/lib/transform-config.js
@@ -4,9 +4,48 @@ import {
 	webpack,
 } from '@webpack-blocks/webpack2';
 
+const getRules = config => [...(config.module.loaders || []), ...(config.module.rules || [])]
+	.map((rule, index) => ({ index, rule }))
+
+const getLoaders = config => getRules(config)
+	.map(({ rule, index }) => ({
+		rule: rule,
+		ruleIndex: index,
+		loaders: (rule.loaders || rule.use || rule.loader)
+	}))
+
+const getPlugins = config => (config.plugins || []).map((plugin, index) => ({ index, plugin }))
+
+const helpers = cwd => ({
+	webpack,
+
+	getRulesByMatchingFile(config, file) {
+		let filePath = path.resolve(cwd, file)
+		return getRules(config)
+			.filter(w => w.rule.test && w.rule.test.exec(filePath))
+	},
+
+	getLoadersByName(config, name) {
+		return getLoaders(config)
+			.map(({ rule, ruleIndex, loaders }) => Array.isArray(loaders)
+				? loaders.map((loader, loaderIndex) => ({ rule, ruleIndex, loader, loaderIndex }))
+				: [{ rule, ruleIndex, loader: loaders, loaderIndex: -1 }]
+			)
+			.reduce((arr, loaders) => arr.concat(loaders), [])
+			.filter(({ loader }) => loader === name || (loader && loader.loader === name))
+	},
+
+	getPluginsByName(config, name) {
+		return getPlugins(config).filter(w => w.plugin && w.plugin.constructor && w.plugin.constructor.name === name)
+	},
+
+	getPluginsByType(config, type) {
+		return getPlugins(config).filter(w => w.plugin instanceof type)
+	},
+})
+
 export default async function (env, config) {
 	const transformerPath = path.resolve(env.cwd, env.config);
-
 	try {
 		await fs.stat(transformerPath);
 	} catch (e) {
@@ -16,5 +55,5 @@ export default async function (env, config) {
 	require('babel-register')();
 	const m = require(transformerPath);
 	const transformer = m && m.default || m;
-	transformer(config, Object.assign({}, env), webpack)
+	transformer(config, Object.assign({}, env), helpers(env.cwd))
 }
