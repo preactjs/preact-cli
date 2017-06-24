05c45220fc51bdaab70c984092860b5a4e9b8545
Rafał Kostrzewski <poczta.raf.kos@gmail.com>
2017-06-24 15:27:19 +0200

    fix: Dependency install loader for sass

--- src/lib/webpack-config.js
+++ src/lib/webpack-config.js
@@ -164,7 +164,7 @@ export default env => {
 						test: /\.s[ac]ss$/,
 						use: [
 							{
-								loader: resolve(__dirname, './npm-install-loader'),
+								loader: resolve(__dirname, './dependency-install-loader'),
 								options: {
 									modules: ['node-sass', 'sass-loader'],
 									save: true
