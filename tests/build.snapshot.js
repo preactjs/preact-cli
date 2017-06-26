58d9815c3b2ca697f27cf4c951d8a7764c3988ba
Rafał Kostrzewski <poczta.raf.kos@gmail.com>
2017-06-26 21:43:30 +0200

    tests: Fix source maps in push manifest & incorrect size comparision
    + tap-diff for pretty output

--- tests/build.snapshot.js
+++ tests/build.snapshot.js
@@ -1,8 +1,10 @@
+const byteDifferenceThreshold = 50;
+
 export const normalize = obj => {
 	let keys = Object.keys(obj);
 
-	if (keys.length === 1 && keys[0] === 'size' && typeof keys[0] === 'number') {
-		return { size: Math.round(obj.size / 10) * 10 };
+	if (keys.length === 1 && keys[0] === 'size' && typeof obj.size === 'number') {
+		return { size: Math.round(obj.size / byteDifferenceThreshold) * byteDifferenceThreshold };
 	}
 
 	return keys.reduce((agg, key) => {
@@ -17,9 +19,10 @@ const smallBuildCommons = {
 		'favicon.ico': { size: 15086 },
 		'icon.png': { size: 51484 }
 	},
-	'polyfills.js': { size: 4068 },
+	'polyfills.js': { size: 4620 },
+	'polyfills.js.map': { size: 31760 },
 	'favicon.ico': { size: 15086 },
-	'sw.js': { size: 3378 },
+	'sw.js': { size: 3330 },
 	'manifest.json': { size: 298 },
 	'push-manifest.json': { size: 2 },
 };
@@ -36,43 +39,53 @@ const fullBuildCommons = {
 			'mstile-150x150.png': { size: 9050 }
 		}
 	},
-	'polyfills.js': { size: 4066 },
+	'polyfills.js': { size: 4620 },
 	'push-manifest.json': { size: 303 },
 	'favicon.ico': { size: 15086 },
 	'manifest.json': { size: 426 },
-	'sw.js': { size: 3905 }
+	'sw.js': { size: 3850 }
 };
 
 export const expectedOutputs = normalize({
 	empty: {
 		...smallBuildCommons,
-		'bundle.js': { size: 10694 },
-		'index.html': { size: 534 },
+		'bundle.js': { size: 9810 },
+		'bundle.js.map': { size: 44660 },
+		'index.html': { size: 630 },
 		'style.css': { size: 131 },
 		'style.css.map': { size: 359 },
 	},
 	simple: {
 		...smallBuildCommons,
-		'bundle.js': { size: 11336 },
-		'index.html': { size: 548 },
+		'bundle.js': { size: 10460 },
+		'bundle.js.map': { size: 48670 },
+		'index.html': { size: 640 },
 		'style.css': { size: 296},
 		'style.css.map': { size: 621 },
 	},
 	root: {
 		...fullBuildCommons,
-		'bundle.js': { size: 18739 },
-		'route-home.chunk.*.js': { size: 959 },
-		'route-profile.chunk.*.js': { size: 1595 },
-		'index.html': { size: 775 },
+		'bundle.js': { size: 18460 },
+		'bundle.js.map': { size: 101500 },
+		'route-home.chunk.*.js': { size: 1020 },
+		'route-home.chunk.*.js.map': { size: 4980 },
+		'route-profile.chunk.*.js': { size: 1660 },
+		'route-profile.chunk.*.js.map': { size: 8610 },
+		'polyfills.js.map': { size: 31750 },
+		'index.html': { size: 870 },
 		'style.css': { size: 1065 },
 		'style.css.map': { size: 2246 },
 	},
 	'default': {
 		...fullBuildCommons,
-		'bundle.js': { size: 19661 },
-		'route-home.chunk.*.js': { size: 961 },
-		'route-profile.chunk.*.js': { size: 1597 },
-		'index.html': { size: 775 },
+		'bundle.js': { size: 19300 },
+		'bundle.js.map': { size: 105590 },
+		'route-home.chunk.*.js': { size: 1000 },
+		'route-home.chunk.*.js.map': { size: 4980 },
+		'route-profile.chunk.*.js': { size: 1650 },
+		'route-profile.chunk.*.js.map': { size: 8610 },
+		'polyfills.js.map': { size: 31800 },
+		'index.html': { size: 850 },
 		'style.css': { size: 1065 },
 		'style.css.map': { size: 2345 },
 	}
