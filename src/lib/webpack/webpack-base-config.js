d2db72c2441c132bf3fb256f4e72c3b2454ae9c7
Hassan Bazzi <contact@hassanbazzi.com>
2017-06-29 11:38:58 -0700

    add ogg and webm support

--- src/lib/webpack/webpack-base-config.js
+++ src/lib/webpack/webpack-base-config.js
@@ -180,7 +180,7 @@ export default (env) => {
 						loader: 'raw-loader'
 					},
 					{
-						test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif|mp4|mov)(\?.*)?$/i,
+						test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif|mp4|mov|ogg|webm)(\?.*)?$/i,
 						loader: isProd ? 'file-loader' : 'url-loader'
 					}
 				]
