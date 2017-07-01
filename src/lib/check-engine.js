0d4e666d65e32e6270aef95efbb1b13e6d849366
Anup <allamsetty.anup@gmail.com>
2017-07-02 00:26:52 +0530

    refactored check-engine.js

--- src/lib/check-engine.js
+++ src/lib/check-engine.js
@@ -1,30 +1,26 @@
-import { execSync } from "child_process";
-
-import semver from "semver";
 import colors from "chalk";
 import pkg from "./../../package.json";
 
-const platformSuffix = process.platform === "win32" ? ".cmd" : "";
-const node = "node" + platformSuffix;
-
 export default function checkVersion() {
-  const requirements = pkg.engines;
-  const nodeVersion = execSync(node + " --version").toString() || '';
+	const version = parseFloat( process.version.substr(1) );
+	const minimum = parseFloat( pkg.engines.node.match(/\d+/g).join('.') );
 
-  if ( !(semver.satisfies(nodeVersion, requirements.node)) ) {
-    const errorMessage = colors.yellow(
-      "\n⚠️ " +
-        "preact-cli requires at least " +
-        "node@" +
-        requirements.node +
-        "\n\n" +
-        "Your node version is " +
-        nodeVersion +
-      "\n"
-    );
-		process.stdout.write(errorMessage) + '\n';
-		process.exit(1);
+  if (version >= minimum) {
+		return true;
   }
 
-	return true;
+	const errorMessage = colors.yellow(
+		"\n⚠️ " +
+			"preact-cli requires at least " +
+			"node@" +
+			minimum +
+			"\n\n" +
+			"Your node version is " +
+			version +
+		"\n"
+	);
+
+	// version not supported && exit
+	process.stdout.write(errorMessage) + '\n';
+	process.exit(1);
 }
