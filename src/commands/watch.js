4f82b50d390a20f1127559ce3a2c068e6a3ab7a2
Anup <allamsetty.anup@gmail.com>
2017-07-30 10:58:56 +0530

    handle watch command to look for HTTPS

--- src/commands/watch.js
+++ src/commands/watch.js
@@ -43,6 +43,8 @@ export default asyncCommand({
 	async handler(argv) {
 		argv.production = false;
 
+		if (process.env.HTTPS) argv.https = true;
+
 		if (argv.https) {
 			let ssl = await getSslCert();
 			if (!ssl) {
