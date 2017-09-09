aadc48576ef07297a13504a91dd7d9c1fcbe79ca
Luke Edwards <luke.edwards05@gmail.com>
2017-09-08 23:11:31 -0700

    modify `AsyncComponent`:
    - dispatch loading & loaded events
    - track `ready` state value
    - always use `child.default` key
    - add `componentDidUpdate` for final event
    - only render _if_ there is a child to render

--- src/components/async.js
+++ src/components/async.js
@@ -1,16 +1,29 @@
 import { h, Component } from 'preact';
 
-export default function(load) {
+function emit(name) {
+	window.dispatchEvent( new Event(name) );
+}
+
+export default function (load) {
 	function Async() {
 		Component.call(this);
+		emit('async-loading');
 		let done = child => {
-			this.setState({ child: child && child.default || child });
+			child = child && child.default;
+			console.log('> child: ', child());
+			this.setState({ child, ready:!!child });
 		};
 		let r = load(done);
 		if (r && r.then) r.then(done);
 	}
 	Async.prototype = new Component;
 	Async.prototype.constructor = Async;
-	Async.prototype.render = (props, state) => h(state.child, props);
+	Async.prototype.componentDidUpdate = function (_, state) {
+		if (!state.ready && this.state.ready) {
+			console.log('LOADED FOR FIRST TIME');
+			emit('async-loaded');
+		}
+	};
+	Async.prototype.render = (props, state) => state.child && h(state.child, props);
 	return Async;
 }
