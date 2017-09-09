a7a8b90820a7d71244ed91a8abc75a87ca1e2ec8
Luke Edwards <luke.edwards05@gmail.com>
2017-09-08 23:15:16 -0700

    complete rework of `entry`
    - use initial render to kick-off async loads
    - manage `inProgress` as escape-hatch var
    - once “loaded” event fires, render to real tree

--- src/lib/entry.js
+++ src/lib/entry.js
@@ -3,8 +3,7 @@ import { h, render } from 'preact';
 if (process.env.NODE_ENV==='development') {
 	// enable preact devtools
 	require('preact/devtools');
-}
-else if ('serviceWorker' in navigator && location.protocol === 'https:') {
+} else if ('serviceWorker' in navigator && location.protocol === 'https:') {
 	navigator.serviceWorker.register('/sw.js');
 }
 
@@ -13,18 +12,39 @@ function getApp() {
 	return h(x && x.default || x);
 }
 
+let inProgress = false;
+let body = document.body;
+let root = body.firstElementChild;
 
-let app = interopDefault(require('preact-cli-entrypoint'));
+function init(first) {
+	if (inProgress) return;
+	console.log('>> inside init', first);
 
-if (typeof app==='function') {
-	let root = document.body.firstElementChild;
+	let app = getApp();
 
-	let init = () => {
-		let app = interopDefault(require('preact-cli-entrypoint'));
-		root = render(h(app), document.body, root);
-	};
-
-	if (module.hot) module.hot.accept('preact-cli-entrypoint', init);
-
-	init();
+	if (first) {
+		console.info('~~> init fake render!');
+		render(app, body.cloneNode(true));
+	} else {
+		console.log('>>> NOT first render, update');
+		root = render(app, body, root);
+		console.log('~> new root', root);
+	}
+	console.log('~~~~~~');
 }
+
+if (module.hot) {
+	module.hot.accept('preact-cli-entrypoint', init);
+}
+
+addEventListener('async-loading', () => {
+	console.log('inside async-loading');
+	(inProgress=true);
+});
+
+addEventListener('async-loaded', () => {
+	console.log('inside async-loaded');
+	inProgress=false; init();
+});
+
+init(true);
