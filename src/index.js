bc62bb456d0ca9d9bcc0cce6eca3dbfaa4302737
Anup <allamsetty.anup@gmail.com>
2017-09-26 22:19:58 +0530

    Remove comments in index.js

--- src/index.js
+++ src/index.js
@@ -26,10 +26,6 @@ yargs
 	.command(watch)
 	.command(serve)
 	.command(list)
-	// .option('cwd', {
-	// 	description: 'A directory to use instead of $PWD.',
-	// 	defaultDescription: '.'
-	// })
 	.usage(logo(`\n\npreact-cli ${pkg.version}`) + `\nFor help with a specific command, enter:\n  preact help [command]`)
 	.help()
 	.alias('h', 'help')
