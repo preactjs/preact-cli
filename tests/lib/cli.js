4a8cb28a9cfdce19467847fda26aab9304034e55
Luke Edwards <luke.edwards05@gmail.com>
2017-12-21 15:30:48 -0800

    simplify tests’ cli wrappers

--- tests/lib/cli.js
+++ tests/lib/cli.js
@@ -1,13 +1,16 @@
 import mkdirp from 'mkdirp';
 import { join, resolve } from 'path';
-import { spawn } from 'child_process';
 import * as cmd from '../../lib/commands';
 import { tmpDir } from './output';
 import { log } from './utils';
 
-const CLI = require.resolve('../../lib');
-const NOINSTALL = !!process.env.SKIP_INSTALL;
-const cliPath = cwd => NOINSTALL ? CLI : resolve(cwd, 'node_modules/.bin/preact');
+const argv = {
+	_: [],
+	src: 'src',
+	dest: 'build',
+	config: 'preact.config.js',
+	prerenderUrls: 'prerender-urls.json',
+};
 
 export async function create(template, name) {
 	let dest = tmpDir();
@@ -18,55 +21,16 @@ export async function create(template, name) {
 
 export function build(cwd) {
 	mkdirp.sync(join(cwd, 'node_modules')); // ensure exists, avoid exit()
-	let src='src', dest='build', config='preact.config.js', json='prerender-urls.json';
-	return cmd.build({ _:[], src, dest, cwd, config, prerenderUrls:json });
+	let opts = Object.assign({ cwd }, argv);
+	return cmd.build(opts);
 }
 
-export const serve = (appDir, port) => log(
-	() => spawnPreact(['serve', port ? `-p=${port}` : undefined], appDir),
-	'preact serve'
-);
+export function serve(cwd, port) {
+	let opts = Object.assign({ cwd, port }, argv);
+	return cmd.watch(opts);
+}
 
-export const watch = (appDir, host, port) => log(
-	() => spawnPreact(['watch', host ? `--host=${host}` : undefined, port ? `-p=${port}` : undefined], appDir),
-	'preact watch'
-);
-
-function spawnPreact(args, cwd) {
-	return new Promise((res, rej) => {
-		let child = spawn('node', [cliPath(cwd), ...args.filter(Boolean)], { cwd });
-		let exitCode, killed = false;
-		let errListener = err => {
-			rej(err);
-		};
-
-		child.on('error', errListener);
-		child.on('exit', code => {
-			killed = true;
-			exitCode = code;
-		});
-
-		let origKill = child.kill.bind(child);
-		child.kill = () => new Promise((res) => {
-			child.stdout.unpipe(process.stdout);
-			child.stderr.unpipe(process.stderr);
-			if (killed) {
-				res(exitCode);
-			} else {
-				child.on('exit', (code) => {
-					res(code);
-				});
-			}
-
-			origKill();
-		});
-
-		child.stdout.pipe(process.stdout);
-		child.stderr.pipe(process.stderr);
-
-		setTimeout(() => {
-			child.removeListener('error', errListener);
-			resolve(child);
-		}, 500);
-	});
+export function watch(cwd, host, port) {
+	let opts = Object.assign({ cwd, host, port, https:false }, argv);
+	return cmd.watch(opts);
 }
