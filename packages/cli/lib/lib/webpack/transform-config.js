7704f871f00a0ce31d8fbfef052671b8989cf1db
Luke Edwards <luke.edwards05@gmail.com>
2018-09-07 10:31:18 -0700

    remove `@babel/register`

--- packages/cli/lib/lib/webpack/transform-config.js
+++ packages/cli/lib/lib/webpack/transform-config.js
@@ -15,11 +15,6 @@ module.exports = async function (env, config, ssr=false) {
 		throw new Error(`preact-cli config could not be loaded!\nFile ${env.config} not found.`);
 	}
 
-	require('@babel/register')({
-		presets: [[require.resolve('@babel/preset-env'), {
-			"targets": { "node": "current" }
-		}]]
-	});
 	const m = require(myConfig);
 	const transformer = m && m.default || m;
 	try {
