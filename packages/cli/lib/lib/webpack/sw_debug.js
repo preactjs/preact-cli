98f0dfcfaa84bcd02f7f8f33b1fc1b013b69b6db
Harshit Kumar <harshit.kumar@quikr.com>
2018-10-10 01:27:55 +0530

    Changing sw_debug logs

--- packages/cli/lib/lib/webpack/sw_debug.js
+++ packages/cli/lib/lib/webpack/sw_debug.js
@@ -3,9 +3,9 @@ self.addEventListener('fetch', function (event) {
     event.respondWith(fetch(event.request).catch(function (err) {
         if (err instanceof TypeError) {
             if (isPostRequest) {
-                console.log('PREACT-CLI-FRIENDLY-MESSAGE(IMPROVEMENT): We saw that your POST API call failed. This might fail for your users as well due to a network error, may be you should look into backgroundSync');
+                console.log('⚛️Preact CLI development tip: A POST request just failed. This might fail for your users as well due to a network error. It may be worth exploring the backgroundSync API.');
             } else {
-                console.log('PREACT-CLI-FRIENDLY-MESSAGE(IMPROVEMENT): We saw that your GET API call failed. This might fail for your users as well due to a network error, may be you should look into runtimeCaching');
+                console.log('⚛️Preact CLI development tip: A GET request just failed. This might fail for your users as well due to a network error. It may be worth adding runtimeCaching to your Service Worker.');
             }
         }
         return err;
