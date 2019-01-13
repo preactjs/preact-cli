1f12ab06cdec7cd266f1462c93211740c3d6a029
Leah <me@hrmny.pw>
2019-01-13 01:34:53 +0100

    add babel macros

--- packages/cli/lib/lib/babel-config.js
+++ packages/cli/lib/lib/babel-config.js
@@ -24,7 +24,8 @@ module.exports = function (env, options={}) {
 			require.resolve('@babel/plugin-transform-react-constant-elements'),
 			isProd && require.resolve('babel-plugin-transform-react-remove-prop-types'),
 			[require.resolve('@babel/plugin-transform-react-jsx'), { pragma: 'h' }],
-			[require.resolve('fast-async'), { spec: true }]
+			[require.resolve('fast-async'), { spec: true }],
+			require.resolve('babel-plugin-macros')
 		].filter(Boolean)
 	};
 };
