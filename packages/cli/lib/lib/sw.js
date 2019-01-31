dc649980811787fd7126001e9d639e84cd40df65
Prateek Bhatnagar <prateek89born@gmail.com>
2019-01-31 13:42:30 -0800

    making changes for workbox v4

--- packages/cli/lib/lib/sw.js
+++ packages/cli/lib/lib/sw.js
@@ -1,7 +1,6 @@
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 
 /* global workbox */
-workbox.precaching.suppressWarnings();
 /** We are sure brotli is enabled for browsers supporting script type=module
  * so we do brotli support only for them.
  * We can do brolti support for other browsers but there is no good way of
@@ -11,44 +10,43 @@ if (process.env.ENABLE_BROTLI && process.env.ES_BUILD) {
 	// Alter the precache manifest to precache brotli files instead of gzip files.
 	self.__precacheManifest = self.__precacheManifest.map(asset => {
 		if (/.*.js$/.test(asset.url)) {
-			asset.url = asset.url.replace(/.esm.js$/, ".esm.js.br");
+			asset.url = asset.url.replace(/.esm.js$/, '.esm.js.br');
 		}
 		return asset;
 	});
 
 	class BrotliRedirectPlugin {
 		// Before saving the response in cache, we need to treat the headers.
-		async cacheWillUpdate({ request, response }) {
-			if (/.js.br$/.test(request.url)) {
+		async cacheWillUpdate({ response }) {
+			if (/.js.br$/.test(response.url)) {
 				const headers = response.headers;
 				const newHeaders = {};
 				for (let key of headers.keys()) {
 					newHeaders[key] = headers.get(key);
 				}
 				// make sure the content type is compatible
-				newHeaders["content-type"] = "application/javascript";
+				newHeaders['content-type'] = 'application/javascript';
 				return new Response(await response.text(), {
-					headers: newHeaders
+					headers: newHeaders,
 				});
 			}
 			return response;
 		}
 	}
-	workbox.precaching.addPlugins(new BrotliRedirectPlugin());
+	workbox.precaching.addPlugins([new BrotliRedirectPlugin()]);
 }
 
 const precacheOptions = {};
 if (process.env.ENABLE_BROTLI) {
-	precacheOptions["urlManipulation"] = ({ url }) => {
+	precacheOptions['urlManipulation'] = ({ url }) => {
 		if (/.esm.js$/.test(url.href)) {
-			url.href = url.href + ".br";
+			url.href = url.href + '.br';
 		}
 		return [url];
 	};
 }
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest, precacheOptions);
-
-workbox.routing.registerNavigationRoute("index.html", {
-	whitelist: [/^(?!\/__).*/]
-});
+workbox.routing.registerNavigationRoute(
+	workbox.precaching.getCacheKeyForURL('/index.html')
+);
