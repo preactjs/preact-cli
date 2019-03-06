fdf81b4074ca73a8eb0902b128859e130049f6a6
Prateek Bhatnagar <prateek89born@gmail.com>
2019-03-05 19:33:37 -0800

    Update run-webpack.js

--- packages/cli/lib/lib/webpack/run-webpack.js
+++ packages/cli/lib/lib/webpack/run-webpack.js
@@ -13,6 +13,7 @@ const { error, isDir, warn } = require('../../util');
 
 async function devBuild(env) {
 	let config = clientConfig(env);
+	
 	await transformConfig(env, config);
 
 	let userPort =
