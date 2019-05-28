e97560c0dd65aaae5a8de7f54202a12cd74583c6
Leah <me@hrmny.sh>
2019-05-28 21:53:20 +0200

    chore: move output cleanup to jest setup script

--- packages/cli/tests/setup.js
+++ packages/cli/tests/setup.js
@@ -1,3 +1,8 @@
+const { join } = require('path');
+const rimraf = require('rimraf');
+
+rimraf.sync(join(__dirname, 'output'));
+
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 240 * 1000;
 
 if (!process.env.LISTENING_TO_UNHANDLED_REJECTION) {
