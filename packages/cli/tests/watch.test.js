c8d168a149fdbc7890ce2fdf3ae91b4eb4e7e692
Prateek Bhatnagar <prateek89born@gmail.com>
2019-07-26 14:53:47 -0700

    reverting watch.js

--- packages/cli/tests/watch.test.js
+++ packages/cli/tests/watch.test.js
@@ -2,14 +2,11 @@ const fs = require('../lib/fs');
 const { resolve } = require('path');
 const startChrome = require('./lib/chrome');
 const { create, watch } = require('./lib/cli');
-const { sleep } = require('./lib/utils');
 
 const { loadPage, waitUntilExpression } = startChrome;
 let chrome, server;
 
 describe('preact', () => {
-	let headers;
-
 	beforeAll(async () => {
 		chrome = await startChrome();
 	});
@@ -28,16 +25,12 @@ describe('preact', () => {
 		let original = await fs.readFile(header, 'utf8');
 		let update = original.replace('<h1>Preact App</h1>', '<h1>Test App</h1>');
 		await fs.writeFile(header, update);
-		await sleep(2000); // forced wait while rebuilding starts
+
 		await waitUntilExpression(
 			page,
 			`document.querySelector('header > h1').innerText === 'Test App'`
 		);
 
-		headers = await page.$$eval('header > h1', nodes =>
-			nodes.map(n => n.innerText)
-		);
-		expect(headers).toEqual(['Test App']);
 		server.close();
 	});
 });
