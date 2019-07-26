499a5d3c3fa0f2479622ed3c562f5ca068e98a80
Prateek Bhatnagar <prateek89born@gmail.com>
2019-07-26 09:36:21 -0700

    Update watch.test.js

--- packages/cli/tests/watch.test.js
+++ packages/cli/tests/watch.test.js
@@ -6,7 +6,7 @@ const { create, watch } = require('./lib/cli');
 const { loadPage, waitUntilExpression } = startChrome;
 let chrome, server;
 
-describe.skip('preact', () => {
+describe('preact', () => {
 	beforeAll(async () => {
 		chrome = await startChrome();
 	});
