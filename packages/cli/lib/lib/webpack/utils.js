87bad6c4b9f5e92fea58ecd965f01c0b2f58bd08
Prateek Bhatnagar <prateek89born@gmail.com>
2020-01-28 14:59:38 -0800

    adding a default true for preactX change

--- packages/cli/lib/lib/webpack/utils.js
+++ packages/cli/lib/lib/webpack/utils.js
@@ -1,7 +1,14 @@
 const { resolve } = require('path');
+const { existsSync } = require('fs');
 
 function isInstalledVersionPreactXOrAbove(cwd) {
-	const pathToPackage = resolve(cwd, 'node_modules', 'preact', 'package.json');
+	const pathToPreact = resolve(cwd, 'node_modules', 'preact');
+	// By default the setup is for Preact X.
+	if (!existsSync(pathToPreact)) {
+		return true;
+	}
+
+	const pathToPackage = resolve(pathToPreact, 'package.json');
 	const { version: preactVersionString } = require(pathToPackage);
 	return (
 		parseInt(preactVersionString.substr(0, preactVersionString.indexOf('.'))) >=
