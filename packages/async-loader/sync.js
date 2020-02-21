e4ddd8f69f0dbc5eaf7fc85efbb3fce051b73065
Prateek Bhatnagar <prateek89born@gmail.com>
2020-02-21 15:38:06 -0800

    bug fix

--- packages/async-loader/sync.js
+++ packages/async-loader/sync.js
@@ -10,9 +10,14 @@ export default function sync(load) {
 		}
 		var modifiedComponent = () => {
 			var c = component(props);
+			if (!c) {
+				return;
+			}
 			if ('props' in c) {
+				c.props = c.props || {};
 				c.props['data-pacr'] = '';
 			} else if ('attributes' in c) {
+				c.attributes = c.attributes || {};
 				c.attributes['data-pacr'] = '';
 			}
 			return c;
