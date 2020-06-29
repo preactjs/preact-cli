b36490c4ee03468bc0581fb36507fbb943fa44e7
Anup <allamsetty.anup@gmail.com>
2020-06-29 19:40:52 +0530

    Hacky fix for async-loader by not modifying this.base or Suspense

--- packages/async-loader/async.js
+++ packages/async-loader/async.js
@@ -2,30 +2,40 @@ import { h, Component } from 'preact';
 
 const PENDING = {};
 
-function Pending() {
-	throw PENDING;
-}
-
 export default function async(load) {
 	let component;
 	function AsyncComponent() {
 		Component.call(this);
+
 		if (!component) {
-			(this.componentWillMount = () => {
-				load(mod => {
+			this.componentWillMount = () => {
+				load((mod) => {
 					component = (mod && mod.default) || mod;
-					this.componentDidCatch = null;
 					this.setState({});
 				});
-			}),
-				(this.componentDidCatch = err => {
-					if (err !== PENDING) throw err;
-				});
+			};
+
 			this.shouldComponentUpdate = () => component != null;
 		}
-		this.render = props => h(component || Pending, props);
+
+		this.render = (props) => {
+			if (component) {
+				return h(component, props);
+			}
+
+			const me = (this.__P || this._parentDom).lastChild;
+
+			return (
+				me &&
+				h(me.localName, {
+					dangerouslySetInnerHTML: PENDING,
+				})
+			);
+		};
 	}
+
 	AsyncComponent.preload = load;
 	(AsyncComponent.prototype = new Component()).constructor = AsyncComponent;
+
 	return AsyncComponent;
 }
