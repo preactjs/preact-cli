b29f98f5485231fb576db90f77c55bd048b91dfe
Teo Dragovic <teo.dragovic@gmail.com>
2020-07-25 10:48:31 +0200

    Add regenerator runtime

--- packages/cli/lib/lib/webpack/polyfills.js
+++ packages/cli/lib/lib/webpack/polyfills.js
@@ -1,2 +1,3 @@
 if (!global.Promise) global.Promise = require('promise-polyfill').default;
 if (!global.fetch) global.fetch = require('isomorphic-unfetch');
+if (!global.regeneratorRuntime) global.regeneratorRuntime = require('regenerator-runtime');
