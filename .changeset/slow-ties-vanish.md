ba2a41d47bc3ef5fa973bbaa49cf6ffff06438a4
Ryan Christian <ryanchristian4427@gmail.com>
2020-09-22 23:52:21 -0500

    fix: Correcting path normalization for service worker

---
+++ .changeset/slow-ties-vanish.md
@@ -0,0 +1,5 @@
+---
+'preact-cli': patch
+---
+
+Fix path normalization for service worker. http://example.comsw.js -> http://example.com/sw.js
--- packages/cli/lib/lib/entry.js
+++ packages/cli/lib/lib/entry.js
@@ -14,17 +14,21 @@ if (process.env.NODE_ENV === 'development') {
 	// only add a debug sw if webpack service worker is not requested.
 	if (process.env.ADD_SW === undefined && 'serviceWorker' in navigator) {
 		// eslint-disable-next-line no-undef
-		navigator.serviceWorker.register(__webpack_public_path__ + 'sw-debug.js');
+		navigator.serviceWorker.register(
+			normalizeURL(__webpack_public_path__) + 'sw-debug.js'
+		);
 	} else if (process.env.ADD_SW && 'serviceWorker' in navigator) {
 		// eslint-disable-next-line no-undef
 		navigator.serviceWorker.register(
-			__webpack_public_path__ + (process.env.ES_BUILD ? 'sw-esm.js' : 'sw.js')
+			normalizeURL(__webpack_public_path__) +
+				(process.env.ES_BUILD ? 'sw-esm.js' : 'sw.js')
 		);
 	}
 } else if (process.env.ADD_SW && 'serviceWorker' in navigator) {
 	// eslint-disable-next-line no-undef
 	navigator.serviceWorker.register(
-		__webpack_public_path__ + (process.env.ES_BUILD ? 'sw-esm.js' : 'sw.js')
+		normalizeURL(__webpack_public_path__) +
+			(process.env.ES_BUILD ? 'sw-esm.js' : 'sw.js')
 	);
 }
 
