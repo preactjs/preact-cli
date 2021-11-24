9010e2a4dd0c8192aebb95426163c783059de109
Ryan Christian <ryanchristian4427@gmail.com>
2021-11-24 05:17:09 -0600

    chore: We don't need the rest of the config

--- packages/cli/tests/subjects/progressive-hydration-preact8/preact.config.js
+++ packages/cli/tests/subjects/progressive-hydration-preact8/preact.config.js
@@ -1,23 +1,7 @@
-const path = require('path');
-
 module.exports = function (config) {
 	const modules = config.resolve.modules;
 	if (modules[0] === 'node_modules') {
 		modules.splice(0, 1);
 		modules.push('node_modules');
-	}
-
-	const loader = path.resolve(
-		__dirname,
-		'../../../../async-loader/async-legacy.js'
-	);
-	const alias = config.resolve.alias;
-	alias['preact-cli/async-component'] = alias['@preact/async-loader/async'] =
-		loader;
-
-	for (const rule of config.module.rules) {
-		if (/async-loader/.test(rule.loader)) {
-			rule.loader = path.resolve(__dirname, '../../../../async-loader/legacy');
-		}
 	}
 };
