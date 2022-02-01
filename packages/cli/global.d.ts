fd74e20d28e570a91ec014b6d1bd358687fd8a5c
Ryan Christian <ryanchristian4427@gmail.com>
2022-01-31 23:26:26 -0600

    fix: Correcting TS global + normalize public path

--- packages/cli/global.d.ts
+++ packages/cli/global.d.ts
@@ -1,6 +1,5 @@
-declare const __webpack_public_path__: string;
-
 declare global {
+	const __webpack_public_path__: string;
 	namespace jest {
 		interface Matchers<R> {
 			toBeCloseInSize(receivedSize: number, expectedSize: number): R;
