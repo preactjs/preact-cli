5e71ade5b7eca8b7af1d959877b9a001c2e8d708
Ryan Christian <rchristian@ryanchristian.dev>
2022-07-08 06:12:36 -0500

    test: Fixing 'create' test suite

--- packages/create-cli/tests/create.test.js
+++ packages/create-cli/tests/create.test.js
@@ -1,7 +1,7 @@
-const { readFile } = require('fs').promises;
+const { readFile } = require('fs/promises');
 const { relative, resolve } = require('path');
-const { create } = require('./lib/cli');
-const { expand } = require('./lib/utils');
+const { create } = require('../../cli/tests/lib/cli');
+const { expand } = require('../../cli/tests/lib/utils');
 const snapshots = require('./images/create');
 
 describe('preact create', () => {
@@ -31,13 +31,5 @@ describe('preact create', () => {
 		const template = await readFile(templateFilePath, 'utf8');
 
 		expect(template.includes('apple-touch-icon')).toEqual(true);
-	});
-
-	it('should fail given an invalid name', async () => {
-		// @ts-ignore
-		const exit = jest.spyOn(process, 'exit').mockImplementation(() => {});
-		await create('simple', '*()@!#!$-Invalid-Name');
-
-		expect(exit).toHaveBeenCalledWith(1);
 	});
 });
