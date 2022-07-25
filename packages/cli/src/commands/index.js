9a3a383a0497c3c9b04ca758032e0b516030cfa1
Ryan Christian <33403762+rschristian@users.noreply.github.com>
2022-07-24 23:05:48 -0500

    refactor: Extracting project creation out to own package (#1708)
    * refactor: Extracting out project creation to own package
    
    * docs: Adding changeset
    
    * refactor: Misc edits to common deps & workspace helper scripts
    
    * test: Fixing 'create' test suite
    
    * ci: Increase minimum Node version
    
    * chore: Fix typos
    
    * docs: Update ReadMe instructions
    
    * chore: Better (and untaken) package name
    
    * docs: Update changeset
    
    * test: Update 'create' test suite & build output hashes
    
    * docs: Expounding upon changeset

--- packages/cli/src/commands/index.js
+++ packages/cli/src/commands/index.js
@@ -1,11 +1,7 @@
 const { build } = require('./build');
-const { create } = require('./create');
-const { list } = require('./list');
 const { watch } = require('./watch');
 
 module.exports = {
 	build,
-	create,
-	list,
 	watch,
 };
