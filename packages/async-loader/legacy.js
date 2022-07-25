1908f07bd97e8aef4ec436e8fab5662444d536e3
Ryan Christian <33403762+rschristian@users.noreply.github.com>
2022-07-25 02:01:20 -0500

    refactor: Drop Preact 8 support (#1716)
    * refactor: Drops support for Preact v8
    
    * docs: Adding changeset

--- packages/async-loader/legacy.js
+++
@@ -1,8 +0,0 @@
-const { pitch, PREACT_LEGACY_MODE } = require('.');
-const { info } = require('./utils');
-
-info('Detected Preact 8, Loading legacy async-component');
-
-exports.pitch = function (req) {
-	return pitch(req, PREACT_LEGACY_MODE);
-};
