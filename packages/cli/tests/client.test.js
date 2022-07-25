df9450a7af0c0983bfd7c499435432f185d1ab3f
Ryan Christian <33403762+rschristian@users.noreply.github.com>
2022-07-25 02:01:20 -0500

    Refactor/remove preact 8 support (#1716)
    * refactor: Drops support for Preact v8
    
    * docs: Adding changeset

--- packages/cli/tests/client.test.js
+++ packages/cli/tests/client.test.js
@@ -22,35 +22,6 @@ describe('client-side tests', () => {
 		PORT = await getPort();
 	});
 
-	//it('should hydrate routes progressively with preact8.', async () => {
-	//	let dir = await subject('progressive-hydration-preact8');
-	//	await build(dir, {}, true);
-	//	const server = getServer(join(dir, 'build'), PORT);
-
-	//	// let page = await loadPage(chrome, `http://127.0.0.1:${PORT}/`);
-	//	const page = await chrome.newPage();
-
-	//	page.on('console', consoleMessage => {
-	//		// eslint-disable-next-line
-	//		console[consoleMessage.type()](consoleMessage.text());
-	//	});
-
-	//	await page.goto(`http://127.0.0.1:${PORT}/`);
-
-	//	// await waitUntilExpression(page, `window.booted`);
-	//	await sleep(500);
-
-	//	const mutations = await page.evaluate('window.ROOT_MUTATION_COUNT');
-
-	//	expect(mutations).toEqual(0);
-
-	//	expect(await page.evaluate('window.CHANGED_VAR')).toEqual(undefined);
-	//	await page.click('button');
-	//	expect(await page.evaluate('window.CHANGED_VAR')).toEqual(1);
-
-	//	server.server.close();
-	//});
-
 	it('should hydrate for pre-rendered URLs only', async () => {
 		let dir = await subject('prerendering-hydration');
 		await build(dir, {});
--- packages/cli/tests/subjects/progressive-hydration-preact8/preact.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (config) {
-	const modules = config.resolve.modules;
-	if (modules[0] === 'node_modules') {
-		modules.splice(0, 1);
-		modules.push('node_modules');
-	}
-};
