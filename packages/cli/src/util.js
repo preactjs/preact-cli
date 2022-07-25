0b31e4c605302d52ecfdb96889efcc14570be8a0
Ryan Christian <33403762+rschristian@users.noreply.github.com>
2022-07-24 23:05:48 -0500

    refactor: Extracting project creation out to own package (#1708)
    * refactor: Extracting out project creation to own package
    
    * docs: Adding changeset
    
    * refactor: Misc edits to common deps & workspace helper scripts
    
    * test: Fixing 'create' test suite
    
    * ci: Increase minimum Node version
    
    * chore: Fix typos
    
    * docs: Update ReadMe instructions
    
    * chore: Better (and untaken) package name
    
    * docs: Update changeset
    
    * test: Update 'create' test suite & build output hashes
    
    * docs: Expounding upon changeset

--- packages/cli/src/util.js
+++ packages/cli/src/util.js
@@ -2,27 +2,10 @@ const { blue, yellow, red } = require('kleur');
 const { normalize, resolve } = require('path');
 const { statSync, existsSync } = require('fs');
 const symbols = require('./symbols');
-const which = require('which');
 const net = require('net');
 
 exports.isDir = function (str) {
 	return existsSync(str) && statSync(str).isDirectory();
-};
-
-exports.dirExists = function (workingDir, destDir) {
-	if (workingDir && destDir) {
-		const target = resolve(workingDir, destDir);
-		return exports.isDir(target);
-	}
-	return false;
-};
-
-exports.hasCommand = function (str) {
-	return !!which.sync(str, { nothrow: true });
-};
-
-exports.trim = function (str) {
-	return str.trim().replace(/^\t+/gm, '');
 };
 
 exports.info = function (text, code) {
@@ -42,14 +25,6 @@ exports.error = function (text, code = 1) {
 
 exports.normalizePath = function (str) {
 	return normalize(str).replace(/\\/g, '/');
-};
-
-exports.normalizeTemplatesResponse = function (repos = []) {
-	return repos.map(repo => ({
-		title: repo.name || '',
-		value: repo.full_name || '',
-		description: repo.description || '',
-	}));
 };
 
 exports.toBool = function (val) {
