cc4a627633ae2f16e6085f4b0612703aa5d5109b
Ryan Christian <33403762+rschristian@users.noreply.github.com>
2022-08-04 04:42:25 -0500

    refactor: Switch to automatic runtime JSX transform (#1725)
    * refactor: Switch over to automatic runtime transform for JSX
    
    * test: Removing `h` imports from test suite
    
    * docs: Adding changeset

--- packages/cli/src/lib/webpack/webpack-base-config.js
+++ packages/cli/src/lib/webpack/webpack-base-config.js
@@ -282,10 +282,6 @@ module.exports = function createBaseConfig(env) {
 						}
 					)
 			),
-			new webpack.ProvidePlugin({
-				h: ['preact', 'h'],
-				Fragment: ['preact', 'Fragment'],
-			}),
 			// Fix for https://github.com/webpack-contrib/mini-css-extract-plugin/issues/151
 			new RemoveEmptyScriptsPlugin(),
 			new MiniCssExtractPlugin({
--- packages/cli/tests/subjects/preload-chunks/routes/route66.js
+++ packages/cli/tests/subjects/preload-chunks/routes/route66.js
@@ -1,4 +1,3 @@
-import { h } from 'preact';
 import Base from './base';
 import './route66.css';
 
