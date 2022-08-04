0c3b31cb203c6b00708733cf5fdb3098533706ce
Ryan Christian <rchristian@ryanchristian.dev>
2022-08-03 23:48:19 -0500

    fix: Corrections for #1708 being merged too early

--- packages/create-cli/lib/index.js
+++ packages/create-cli/lib/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const sade = require('sade');
 const { create } = require('./commands/create.js');
-//const { create } = require('./commands/create.js)';
+const { list } = require('./commands/list.js');
 const { error } = require('./util.js');
 
 const prog = sade('create-preact-app').version('0.1.3');
@@ -16,10 +16,7 @@ prog
 	.option('--git', 'Initialize git repository', true)
 	.action(create);
 
-//prog
-//    .command('list')
-//    .describe('List official templates')
-//    .action(list);
+prog.command('list').describe('List official templates').action(list);
 
 prog.parse(process.argv, {
 	unknown: arg => {
