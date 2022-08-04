48d2670c6597357a0835833e47b7953814aec65c
Ryan Christian <rchristian@ryanchristian.dev>
2022-08-04 04:13:11 -0500

    test: Removing `h` imports from test suite

--- packages/cli/tests/subjects/css-inline/index.js
+++ packages/cli/tests/subjects/css-inline/index.js
@@ -1,5 +1,3 @@
-import { h } from 'preact';
-
 import './style.css';
 
 export default () => <h1>Test CSS inlining</h1>;
--- packages/cli/tests/subjects/static-root/index.js
+++ packages/cli/tests/subjects/static-root/index.js
@@ -1,5 +1,3 @@
-import { h } from 'preact';
-
 export default () => {
 	return <h1>Static test</h1>;
 };
